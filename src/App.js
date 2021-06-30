419ae540880e8e62a8332c81c0ff842d4b0c5dbd
Meklit Cherie <makywonde347@yahoo.com>
2021-06-29 18:33:52 -0700

    setting up redux

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
+// import { createStore } from 'redux'
+// import { Provider } from 'react-redux'
+// import reducers from './reducers'
+
 import './App.css';
+
+
+// const store = createStore(reducers)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <Provider store={store}>
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Africa Quiz</h1>
+        </header>
+      </div>
+    // </Provider>
   );
 }
 
