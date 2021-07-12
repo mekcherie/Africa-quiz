4f433e8ce1b15eb57ba7420fea5c1639777107e1
Meklit Cherie <makywonde347@yahoo.com>
2021-07-12 13:38:17 -0700

    adding an action file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
+import React, {useState } from 'react';
 import FlashcardList from './FlashcardList';
 import questions from './questions';
 import Header from './Header';
 import './flashcard.css';
 
 function App() {
-
-  return (
-    <div className="App">
-      <FlashcardList />
-      <flashcard />
-      <questions />
-      <Header />
-    </div>
-  );
-}
+   const [flashcards, setFlashcards ] = useState(questions,  Header)
+    return (
+      <FlashcardList flashcards={flashcards} />
+    );
+  }
+  export default App;
+  // return (
+//     <div className="App">
+//       <FlashcardList />
+//       <flashcard />
+//       <questions />
+//       <Header />
+//     </div>
+//   );
+// }
 //   const [flashcards, setFlashcards ] = useState(questions,  Header)
 //   return (
 //     <FlashcardList flashcards={flashcards} />
 //   );
 // }
 
-export default App;
